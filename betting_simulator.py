d2bd3a97ffa93d06e058fe116a611bb6da9bed43
Alex Xin <xinba1989918@gmail.com>
2018-07-12 00:23:32 +1000

    add shuffle to betting data

--- betting_simulator.py
+++ betting_simulator.py
@@ -1,6 +1,6 @@
 from strategy_factory import StrategyFactory
 from visualization_factory import VisualizationFactory
-from config import betting_settings, visualization_settings
+from config import betting_settings, visualization_settings, betting_data_settings
 from exception import StrategyNotFoundException
 import logging
 from random import shuffle
@@ -17,7 +17,7 @@ class Simulator:
         if self.betting_strategy is None:
             raise StrategyNotFoundException('strategy not found, make sure strategy class exist')
         else:
-            self._load_data()
+            self._load_data(betting_data_settings)
 
     def run(self):
         try:
@@ -29,14 +29,16 @@ class Simulator:
                 self.visualization.generate_graph({'x':list(range(len(results))), 'y':[result['current_money'] for result in results]})
                 self.visualization.save()
 
-    def _load_data(self):
+    def _load_data(self, betting_data_settings):
         results = []
         odds_ratios = []
         odds = []
         with open('betting data/processed result/processed_result.csv','r') as fr:
             lines = [line for line in fr]
 
-        shuffle(lines)
+        if betting_data_settings.get('shuffle', None):
+            shuffle(lines)
+
         for line in lines:
             result, odds_ratio, odd_home, odd_away = line.split(",")
             results.append(result)
--- config.py
+++ config.py
@@ -1,8 +1,12 @@
 import os
 BASE_DIR = os.path.dirname((os.path.abspath(__file__)))
 
+betting_data_settings = {
+    'shuffle': True
+}
+
 betting_settings = {
-    'strategy': 'double_betting_after_lossing',
+    'strategy': 'double_betting_after_winning',
     'initial_money': 10000,
     'initial_bet': 100
 }
@@ -10,6 +14,6 @@ betting_settings = {
 visualization_settings = {
     'plugin': 'money_history',
     'title': 'betting history',
-    'file': BASE_DIR + '/simulation_result/visualization/double_betting_after_lossing.html'
+    'file': BASE_DIR + '/simulation_result/visualization/double_betting_after_winning.html'
 }
 
