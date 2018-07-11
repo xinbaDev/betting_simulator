eb9884dbb6190d2d506674b2dcb20f14e24f3853
Alex Xin <xinba1989918@gmail.com>
2018-07-11 23:57:19 +1000

    add double_betting_after_winning strategy & update .gitignore

---
+++ strategies/double_betting_after_winning.py
@@ -0,0 +1,52 @@
+from .strategy import Strategy
+from utils import configure_logger
+
+logger = configure_logger("betting", "DoubleBettingAfterWinning.csv")
+
+class DoubleBettingAfterWinning(Strategy):
+
+    max_double_betting_times = 3
+    double_betting_times = 0
+
+    def exceute(self):
+        current_bet = self.initial_bet
+        current_money = self.initial_money
+        betting_history = []
+        for index, odd in enumerate(self.odds):
+            if not self._betting_condition(self.odds_ratio[index]):
+                continue
+
+            current_money = self._bet(current_money, current_bet)
+            if current_money <= 0:
+                return betting_history
+
+            if self.result[index] == 'win':
+                current_money += (current_bet * float(odd))
+                current_bet = self._cal_bet_after_winning(current_bet)
+            else:
+                current_bet = self._cal_bet_after_lossing()
+
+            logger.info("Index:{}, current_money:{}, current_bet:{}, odds:{}".format(index, current_money,current_bet, odd))
+            betting_history.append({'index':index, 'current_money':current_money, 'current_bet':current_bet})
+
+        return betting_history
+
+    # betting according to odd_ratio(odd_home/odd_away)
+    def _betting_condition(self, odd_ratio):
+        return 0.5 < float(odd_ratio) and float(odd_ratio) < 1
+
+    def _bet(self, current_money, current_bet):
+        current_money -= current_bet
+        return current_money
+
+    def _cal_bet_after_winning(self, current_bet):
+        if self.double_betting_times < self.max_double_betting_times:
+            self.double_betting_times += 1
+            return current_bet * 2
+        else:
+            self.double_betting_times = 0
+            return self.initial_bet
+
+    def _cal_bet_after_lossing(self):
+        self.double_betting_times = self.max_double_betting_times
+        return self.initial_bet
